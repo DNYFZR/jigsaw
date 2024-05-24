6e69c8165936e173cad9ff2148192a7d265a7c7a
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-05-24 16:53:50 +0100

    Updated homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -19,21 +19,17 @@ const NewsTicker: React.FC<NewsProps> = ({ headlines }) => {
 };
 
 const headlines = [
-  "PLY-AI : Use Open Source AI", 
+  "JIGSAW : Use Open Source AI", 
   "Powered by HugginFace Transformers.js",
   "Text Summarization : get the key points of any text", 
-  "Object Detection : analyse the content of an image",
-  "Document Q&A : ask the AI about your documents", 
-  "Text to Audio : generate audio samples from a text description", 
-  "Text to SQL : generate SQL queries by description",
+  "Image Analyser : analyse & summarise the content of an image",
 ]
 
 const Homepage: React.FC = () => {
     return(
       <>
         <NewsTicker headlines={headlines}/>
-        <h3>PLY-AI</h3>
-        <h4>Access Open Source Large Language Models (LLMs)</h4>
+        <h4>Open Source AI Tools User Interface</h4>
         
         <div className='list-container'>
           <ul><b>The app is currently capable of :</b>
@@ -41,18 +37,15 @@ const Homepage: React.FC = () => {
             <li>Image Object Detection</li>
             <li>Image Classification</li>
           </ul>  
-          
-          <ul><b>We are also currently working on :</b>
-            <li>Text to SQL</li>
-            <li>Text to Audio</li>
-            <li>Document Q&A</li>
-          </ul>  
         </div>
 
-        <p>
-          If you have a feature request, or there is an issue with any part of the application, please use the contact form to let us know. 
-        </p>
-       
+        <div className='list-container'>
+          <ul><b>We are also currently working on :</b>
+            <li>Document Q&A</li>
+            <li>Text to SQL</li>
+            <li>Text to Audio</li>
+          </ul>  
+        </div>       
       </>
     )
   };
